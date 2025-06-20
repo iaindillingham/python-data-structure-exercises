17878457c944dbaed28a762e5d55cc827249b74e
Iain Dillingham <iain@dillingham.me.uk>
2025-06-20 16:06:47 +0100

    Reformat with Ruff

--- football_results.py
+++ football_results.py
@@ -9,23 +9,23 @@
 # NB Teams score three points for a win and one point for a draw.
 
 results = [
-        {'Austria': 0, 'Hungary': 2},
-        {'Portugal': 1, 'Iceland': 1},
-        {'Iceland': 1, 'Hungary': 1},
-        {'Portugal': 0, 'Austria': 0},
-        {'Iceland': 2, 'Austria': 1},
-        {'Hungary': 3, 'Portugal': 3},
+    {"Austria": 0, "Hungary": 2},
+    {"Portugal": 1, "Iceland": 1},
+    {"Iceland": 1, "Hungary": 1},
+    {"Portugal": 0, "Austria": 0},
+    {"Iceland": 2, "Austria": 1},
+    {"Hungary": 3, "Portugal": 3},
 ]
 
-print('There were {} matches in the group'.format(len(results)))
+print("There were {} matches in the group".format(len(results)))
 
 # TODO: Write code to answer the following questions:
 
-print('The match with the most goals was', '?')
-print('The match with the fewest goals was', '?')
-print('The team with the most total goals was', '?')
-print('The team with the fewest total goals was', '?')
-print('The team with the most points was', '?')
-print('The team with the fewest points was', '?')
+print("The match with the most goals was", "?")
+print("The match with the fewest goals was", "?")
+print("The team with the most total goals was", "?")
+print("The team with the fewest total goals was", "?")
+print("The team with the most points was", "?")
+print("The team with the fewest points was", "?")
 
 # TODO (extra): Write code to compute and display a league table
